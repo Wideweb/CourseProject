319fcf6913d8e724e826f7a5d2da7994ccdc1ecb
Wideweb <alckevich@live.com>
2014-11-22 22:39:02 +0300

    Network has been teached(Arial, TimesNewRoman, Adobe Ming Std L). Result makes me sad.

--- CourseProject/Program.cs
+++ CourseProject/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Drawing.Text;
+using System.Drawing.Drawing2D;
 
 namespace CourseProject
 {
@@ -11,19 +13,48 @@ namespace CourseProject
     {
         static void Main(string[] args)
         {
-            Bitmap bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\A.bmp"));
-            var vector = GetVector(bmp);
             KohonenNetwork kn = new KohonenNetwork();
-            kn.Study(vector, 0);
+            Bitmap bmp;
+            int[] vector;
+            char symbol;
 
-            bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\B.bmp"));
-            vector = GetVector(bmp);
-            kn.Study(vector, 1);
+            for (var i = 0; i < 26; i++)
+            {
+                symbol = (char)('A' + i);
+                bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\Arial\" + symbol + ".bmp"));
+                vector = GetVector(bmp);
+                kn.Study(vector, i);
+            }
+            
+            for (var i = 0; i < 26; i++)
+            {
+                symbol = (char)('A' + i);
+                bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\TimesNewRoman\" + symbol + ".bmp"));
+                vector = GetVector(bmp);
+                kn.Study(vector, i);
+            }
+            
+            for (var i = 0; i < 26; i++)
+            {
+                symbol = (char)('A' + i);
+                bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\Adobe Ming Std L\" + symbol + ".bmp"));
+                vector = GetVector(bmp);
+                kn.Study(vector, i);
+            }
+            
+            for (var i = 0; i < 26; i++)
+            {
+                symbol = (char)('A' + i);
+                bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\Arial\" + symbol + ".bmp"));
+                vector = GetVector(bmp);
+                Console.WriteLine(symbol + " - " + (char)('A' + kn.Parse(vector)));
+            }
 
-            bmp = new Bitmap(Bitmap.FromFile(@"C:\Users\Alexander\Desktop\A-.bmp"));
-            vector = GetVector(bmp);
-
-            Console.WriteLine(kn.Parse(vector));
+            /*for (var i = 0; i < 26; i++)
+            {
+                symbol = (char)('A' + i);
+                CreateBitmapImage(symbol.ToString());
+            }*/
             
             Console.ReadKey();
         }
@@ -37,7 +68,7 @@ namespace CourseProject
                         vector[i * 45 + j] = 1;
                     else
                         vector[i * 45 + j] = 0;
-
+            
             return vector;
         }
 
@@ -47,6 +78,45 @@ namespace CourseProject
                 return true;
             else
                 return false;
+        }
+
+        static Bitmap CreateBitmapImage(string imageText)
+        {
+            Bitmap bitmap = new Bitmap(1, 1);
+
+            int width = 0;
+            int height = 0;
+
+            // Создаем объект Font для "рисования" им текста.
+            Font font = new Font("Adobe Ming Std L", 58, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+
+            // Создаем объект Graphics для вычисления высоты и ширины текста.
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            // Определение размеров изображения.
+            int StrWidth = (int)graphics.MeasureString(imageText, font).Width;
+            int StrHeight = (int)graphics.MeasureString(imageText, font).Height;
+            width = 45;
+            height = 45;
+
+            // Пересоздаем объект Bitmap с откорректированными размерами под текст и шрифт.
+            bitmap = new Bitmap(bitmap, new Size(width, height));
+
+            // Пересоздаем объект Graphics
+            graphics = Graphics.FromImage(bitmap);
+
+            // Задаем цвет фона.
+            graphics.Clear(Color.White);
+            // Задаем параметры анти-алиасинга
+            graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+            // Пишем (рисуем) текст
+            graphics.DrawString(imageText, font, new SolidBrush(Color.FromArgb(0, 0, 0)), (45 - StrWidth) / 2, (45 - StrHeight)/2 + 3);
+            graphics.Flush();
+
+            bitmap.Save(@"C:\Users\Alexander\Desktop\Adobe Ming Std L\" + imageText + ".bmp");
+
+            return (bitmap);
         }
     }
 }
