ca236a012ac6c78047c40d178877be68e8a917a7
Wideweb <alckevich@live.com>
2014-11-22 17:17:39 +0300

    Code is not executable

---
+++ CourseProject/KohonenNetwork.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class KohonenNetwork
+    {
+        private readonly Input[] _inputs;
+        private readonly Neuron[] _neurons;
+
+        public int Parse(int[] input)
+        {
+            for (var i = 0; i < _inputs.Length; i++)
+            {
+                var inputNeuron = _inputs[i];
+                foreach (var outgoingLink in inputNeuron.OutgoingLinks)
+                {
+                    outgoingLink.Neuron.Power += outgoingLink.Weight * input[i];
+                }
+            }
+            var maxIndex = FindNeuronWithMaxExcitation();
+            RemoveExcitation();
+
+            return maxIndex;
+        }
+
+        private int FindNeuronWithMaxExcitation()
+        {
+            var maxIndex = 0;
+            for (var i = 1; i < _neurons.Length; i++)
+            {
+                if (_neurons[i].Power > _neurons[maxIndex].Power)
+                    maxIndex = i;
+            }
+            return maxIndex;
+        }
+
+        private void RemoveExcitation()
+        {
+            foreach (var outputNeuron in _neurons)
+            {
+                outputNeuron.Power = 0;
+            }
+        }
+
+        public void Study(int[] input, int correctAnswer)
+        {
+            var neuron = _neurons[correctAnswer];
+            for (var i = 0; i < neuron.IncomingLinks.Length; i++)
+            {
+                var incomingLink = neuron.IncomingLinks[i];
+                incomingLink.Weight = 0.5 * (input[i] + incomingLink.Weight);
+            }
+        }
+
+
+    }
+}
---
+++ CourseProject/Neuron.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseProject
+{
+    class Neuron
+    {
+        public Link[] IncomingLinks;
+        public double Power { get; set; }
+    }
+}
